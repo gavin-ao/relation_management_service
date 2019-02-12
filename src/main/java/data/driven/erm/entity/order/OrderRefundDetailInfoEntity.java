4b917c7993964e81aa58a3822a35fd029bf50135
liuxiaolei319 <182150305@qq.com>
2019-02-12 11:38:41 +0800

    增加字段 lxl

--- src/main/java/data/driven/erm/entity/order/OrderRefundDetailInfoEntity.java
+++ src/main/java/data/driven/erm/entity/order/OrderRefundDetailInfoEntity.java
@@ -66,8 +66,40 @@ public class OrderRefundDetailInfoEntity {
      */
     private Date createAt;
 
+    /**
+     * 商品名称
+     */
+    private String commodityName;
+
+    /**
+     * 交易时间
+     */
+    private Date transactionDate;
+
+    /**
+     * 处理方式
+     */
+    private Integer processingMode;
+
+    /**
+     * 退款原因
+     */
+    private Integer refundReason;
+
+    /**
+     * 手机号码
+     */
+    private String mobilePhone;
+
+    /**
+     * 备注信息
+     */
+    private String remarks;
+
     public OrderRefundDetailInfoEntity(String appid, String wechatUserId, String storeId, String transactionId,
-                                       String outRefundNo, String outTradeNo, BigDecimal totalFee, BigDecimal refundFee) {
+                                       String outRefundNo, String outTradeNo, BigDecimal totalFee, BigDecimal refundFee,
+                                       String commodityName,Date transactionDate,Integer processingMode,
+                                       Integer refundReason,String mobilePhone,String remarks) {
         this.appid = appid;
         this.wechatUserId = wechatUserId;
         this.storeId = storeId;
@@ -76,6 +108,60 @@ public class OrderRefundDetailInfoEntity {
         this.outTradeNo = outTradeNo;
         this.totalFee = totalFee;
         this.refundFee = refundFee;
+        this.commodityName = commodityName;
+        this.transactionDate = transactionDate;
+        this.processingMode = processingMode;
+        this.refundReason = refundReason;
+        this.mobilePhone = mobilePhone;
+        this.remarks = remarks;
+    }
+
+    public String getCommodityName() {
+        return commodityName;
+    }
+
+    public void setCommodityName(String commodityName) {
+        this.commodityName = commodityName;
+    }
+
+    public Date getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(Date transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    public Integer getProcessingMode() {
+        return processingMode;
+    }
+
+    public void setProcessingMode(Integer processingMode) {
+        this.processingMode = processingMode;
+    }
+
+    public Integer getRefundReason() {
+        return refundReason;
+    }
+
+    public void setRefundReason(Integer refundReason) {
+        this.refundReason = refundReason;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
     }
 
     public String getOrderRefundDetailInfoId() {
