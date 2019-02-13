381413bd0040552f53ed0f0c15f65c5aa434400a
liuxiaolei319 <182150305@qq.com>
2019-02-13 11:10:54 +0800

    增加退款图片表Service、Impl并添加新增方法 lxl

---
+++ src/main/java/data/driven/erm/business/order/OrderRefundImageService.java
@@ -0,0 +1,22 @@
+package data.driven.erm.business.order;
+
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * @Author: lxl
+ * @describe 退款图片表Service
+ * @Date: 2019/2/13 11:00
+ * @Version 1.0
+ */
+public interface OrderRefundImageService {
+
+    /**
+     * @description 新增退款图片表
+     * @author lxl
+     * @date 2019-02-13 11:03
+     * @param orderRefundDetailInfoId 订单退款详情信息表id
+     * @param pictureId 图片id
+     * @return
+     */
+    JSONObject insertOrderRefundImageEntity(String orderRefundDetailInfoId,String pictureId);
+}
