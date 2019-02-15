3ec4e490ba845c2dc6e3cfcce47f1ad9b337a029
liuxiaolei319 <182150305@qq.com>
2019-02-15 16:19:42 +0800

    增加"新增属性同时新增属性值"方法 lxl

--- src/main/java/data/driven/erm/business/attribute/AttrInfoService.java
+++ src/main/java/data/driven/erm/business/attribute/AttrInfoService.java
@@ -1,5 +1,11 @@
 package data.driven.erm.business.attribute;
 
+import com.alibaba.fastjson.JSONObject;
+import data.driven.erm.vo.attribute.AttrInfoVO;
+
+import javax.xml.bind.util.JAXBSource;
+import java.util.List;
+
 /**
  * @Author: lxl
  * @describe 属性表Sercice
@@ -16,4 +22,47 @@ public interface AttrInfoService {
      * @return 
      */
     String insertCode(Integer attrType);
+
+    /**
+     * @description 获取属性名称和属性值列表
+     * @author lxl
+     * @date 2019-02-15 15:09
+     * @param attrType 属性类型
+     * @return
+     */
+    AttrInfoVO getAttrInfoAndValue(Integer attrType);
+
+    /**
+     * @description 新增属性同时新增属性值
+     * @author lxl
+     * @date 2019-02-15 15:38
+     * @param attrName 属性名称
+     * @param attrValueList 属性值列表
+     * @param attrType 属性类型 0 描述 1 规格
+     * @return 
+     */
+    JSONObject insertAtrrInfoAndValue(String attrName, List<String> attrValueList,Integer attrType);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
