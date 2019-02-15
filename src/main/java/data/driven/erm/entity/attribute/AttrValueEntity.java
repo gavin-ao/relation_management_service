6212746ed0b1caa06784123ec6c58b5b5b3662e6
liuxiaolei319 <182150305@qq.com>
2019-02-15 14:59:23 +0800

    增加属性表、属性值表、品牌属性实体类 lxl

---
+++ src/main/java/data/driven/erm/entity/attribute/AttrValueEntity.java
@@ -0,0 +1,101 @@
+package data.driven.erm.entity.attribute;
+
+import java.util.Date;
+
+/**
+ * @Author: lxl
+ * @describe 属性值实体
+ * @Date: 2019/2/15 11:59
+ * @Version 1.0
+ */
+public class AttrValueEntity {
+    /**
+     * 主键
+     */
+    private String attrValueId;
+
+    /**
+     * 属性值
+     */
+    private String attrValue;
+
+    /**
+     * 属性表id
+     */
+    private String attrId;
+
+    /**
+     * 创建时间
+     */
+    private Date createAt;
+
+    /**
+     * 修改时间
+     */
+    private Date updateAt;
+
+    public String getAttrValueId() {
+        return attrValueId;
+    }
+
+    public void setAttrValueId(String attrValueId) {
+        this.attrValueId = attrValueId;
+    }
+
+    public String getAttrValue() {
+        return attrValue;
+    }
+
+    public void setAttrValue(String attrValue) {
+        this.attrValue = attrValue;
+    }
+
+    public String getAttrId() {
+        return attrId;
+    }
+
+    public void setAttrId(String attrId) {
+        this.attrId = attrId;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
