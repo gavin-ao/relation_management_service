f4f78b013dda915c2b568aaae88ac5fff03fe92d
liuxiaolei319 <182150305@qq.com>
2019-02-15 19:43:05 +0800

    修改获取属性名称和属性值列表测试方法 lxl

--- src/test/java/data/driven/erm/business/attribute/AttrInfoServiceTest.java
+++ src/test/java/data/driven/erm/business/attribute/AttrInfoServiceTest.java
@@ -39,19 +39,22 @@ public class AttrInfoServiceTest extends UnitTestBase {
      * @date 2019-02-15 15:27
      * @return
      */
-//    @Test
-//    public void getAttrInfoAndValue(){
-//        AttrInfoVO attrInfoVO = attrInfoService.getAttrInfoAndValue(0);
-//        System.out.println("属性表ID："+attrInfoVO.getAttrId());
-//        System.out.println("属性名称："+attrInfoVO.getAttrName());
-//        System.out.println("属性类型："+attrInfoVO.getAttrType());
-//        System.out.println("属性码："+attrInfoVO.getAttrCode());
-//        List<AttrValueEntity> attrValueEntityList = attrInfoVO.getAttrValueEntityList();
-//        for (AttrValueEntity attrValueEntity : attrValueEntityList){
-//            System.out.println("属性值表ID：" + attrValueEntity.getAttrValueId());
-//            System.out.println("属性值：" + attrValueEntity.getAttrValue());
-//        }
-//    }
+    @Test
+    public void getAttrInfoAndValue(){
+        List<AttrInfoVO> attrInfoVOList = attrInfoService.getAttrInfoAndValue(0);
+
+        for (AttrInfoVO attrInfoVO : attrInfoVOList){
+            List<AttrValueEntity> attrValueEntityList = attrInfoVO.getAttrValueEntityList();
+            System.out.println("属性表ID："+attrInfoVO.getAttrId());
+            System.out.println("属性名称："+attrInfoVO.getAttrName());
+            System.out.println("属性类型："+attrInfoVO.getAttrType());
+            System.out.println("属性码："+attrInfoVO.getAttrCode());
+            for (AttrValueEntity attrValueEntity : attrValueEntityList){
+                System.out.println("属性值表ID：" + attrValueEntity.getAttrValueId());
+                System.out.println("属性值：" + attrValueEntity.getAttrValue());
+            }
+        }
+    }
 
     /**
      * @description 新增属性同时新增属性值
@@ -86,9 +89,9 @@ public class AttrInfoServiceTest extends UnitTestBase {
      * @date 2019-02-15 16:24
      * @return
      */
-    @Test
-    public void updateAttrName(){
-        JSONObject resultJson = attrInfoService.updateAttrName("5c66744c7053bb1fd80bffc9","产地");
-        System.out.println(resultJson.getString("msg"));
-    }
+//    @Test
+//    public void updateAttrName(){
+//        JSONObject resultJson = attrInfoService.updateAttrName("5c66744c7053bb1fd80bffc9","产地");
+//        System.out.println(resultJson.getString("msg"));
+//    }
 }
